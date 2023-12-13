17eaf4dcb50297903c87c756a27aa5ed1a3d5ab1
Elizaveta <aliza-97@mail.ru>
2023-12-13 23:24:11 +0300

    Reformat code

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -1,8 +1,8 @@
 package ru.netology.statistic;
 
-        import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Test;
 
-        import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Assertions;
 
 public class StatisticsServiceTest {
 
@@ -18,15 +18,15 @@ public class StatisticsServiceTest {
         Assertions.assertEquals(expected, actual);
     }
 
-@Test
+    @Test
     public void findSimilar() {
-    StatisticsService service = new StatisticsService();
+        StatisticsService service = new StatisticsService();
 
-    long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-    long expected = 12;
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
 
-    long actual = service.findMax(incomesInBillions);
+        long actual = service.findMax(incomesInBillions);
 
-    Assertions.assertEquals(expected, actual);
-}
+        Assertions.assertEquals(expected, actual);
+    }
 }
